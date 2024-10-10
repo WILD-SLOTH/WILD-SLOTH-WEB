a6024dedee04fbcf630cd2e9a1223484eb9d2118
BenSchepflin <Schepflin.8@osu.edu>
2024-10-09 20:13:25 -0400

    Order issue

---
+++ frontend/src/Orders/data.tsx
@@ -0,0 +1,38 @@
+export default {
+    orders: [{
+        _id: 1,
+        createdAt: '02/20/2021',
+        totalPrice: 42.90,
+        user: {
+            name: 'Taylor'
+        },
+        ispaid: true,
+        paidAt: '02/14/2021',
+        isDelivered: false,
+        DeliveredDate: null,
+    },
+    {
+        _id: 2,
+        createdAt: '02/20/2021',
+        totalPrice: 42.90,
+        user: {
+            name: 'Pedro'
+        },
+        ispaid: true,
+        paidAt: '02/14/2021',
+        isDelivered: false,
+        DeliveredDate: null,
+    },
+    {
+        _id: 3,
+        createdAt: '02/20/2021',
+        totalPrice: 42.90,
+        user: {
+            name: 'Sanjay'
+        },
+        ispaid: true,
+        paidAt: '02/14/2021',
+        isDelivered: false,
+        DeliveredDate: null,
+    }]    
+}
